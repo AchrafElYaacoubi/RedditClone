a4995c65e29d417ca6ed511e094350e40b10856b
AchrafElYaacoubi <achraf18el@gmail.com>
2020-08-31 07:07:19 +0100

    importing passport strategies

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const config = require('./config');
+const passport = require('passport');
+const { localStrategy, localAuth } = require('./auth/local');
+const { jwtStrategy, jwtAuth } = require('./auth/jwt');
+
 
 
 const app = express();
@@ -9,11 +13,20 @@ const app = express();
 app.use(express.json());
 app.use(morgan('common'));
 
+app.use(passport.initialize());  // initialize passport
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+// Implementing the strategie on a specific route
+app.use("/login", localAuth, require('./routes/auth'))
+
+
 connect()
   .on('error', console.log)
   .once('open', listen)
 
-app.use('/posts', require('./routes/posts'));
+app.use('/posts', jwtAuth, require('./routes/posts'));
 app.use('/users', require('./routes/users'));
 
 app.get('*', (req, res) => {
@@ -29,7 +42,8 @@ function listen() {
 function connect () {
   const options = { 
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true,
   };
   mongoose.connect(config.db, options);
   return mongoose.connection;
