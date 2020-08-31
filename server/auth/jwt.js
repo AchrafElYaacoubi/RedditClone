22bd96149d13e8e43f7506fa6e4f6c2e514deb1b
AchrafElYaacoubi <achraf18el@gmail.com>
2020-08-31 07:06:32 +0100

    adding passport strategies

---
+++ server/auth/jwt.js
@@ -0,0 +1,22 @@
+const passport = require("passport");
+const { Strategy: JwtStrategy } = require('passport-jwt');
+const { ExtractJwt } = require('passport-jwt');
+const config = require('../config');
+
+
+
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.jwt.secret;
+
+const jwtStrategy = new JwtStrategy(opts, function(jwt_payload, done) {
+  done(false, jwt_payload)
+})
+
+const jwtAuth = passport.authenticate('jwt',
+  { 
+    session: false,
+  }
+)
+
+module.exports = { jwtStrategy, jwtAuth };
---
+++ server/auth/local.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+const { Strategy: LocalStrategy } = require('passport-local');
+const User = require("../models/user");
+const jwt = require('jsonwebtoken');
+
+
+const localStrategy = new LocalStrategy( async (username, password, done) => {
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return done(null, false, { message: 'User not found' });
+
+    const valid = await user.verifyPassword(password);
+    if (!valid) return done(null, false, { message: 'Invalid password' });
+
+    return done(null, user);
+  } catch (err) {
+    done(err);
+  }
+})
+
+const localAuth = passport.authenticate('local',
+  { 
+    session: false,
+  }
+)
+
+module.exports = { localStrategy, localAuth};
